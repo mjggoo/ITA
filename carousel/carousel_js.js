d6a808e403b873553461beeb34bb12870dfa7b78
Rose <34325092+mjggoo@users.noreply.github.com>
2020-08-10 17:54:15 +0900

    carousel_update

--- carousel/carousel_js.js
+++ carousel/carousel_js.js
@@ -4,21 +4,68 @@ let slides = document.querySelector(".slides");
 let slide = document.querySelectorAll(".slides li");
 let slideWidth = 320 , slideMargin = 30;
 let currentIndex =0;
-let slideCount = slide.length;
-let btn  = document.querySelectorAll(".btn");
+let slideCount = 3;
+let btn = document.querySelectorAll(".btn_wrapper .btns .btn");
+let i=0; 
 
-slides.style.width = slideWidth*3  + slideMargin*2 + "px";
+for(i = 0; i < 3 ; i++){
+    btn[i].addEventListener('click', function(){
+        pageScroll(i);
+    });
+}
+
+function pageScroll(i){
+    if(i != currentIndex && i > currentIndex){
+        moveSlide(currentIndex+1);
+        console.log(i + "," + currentIndex)
+    }
+    else if(i != currentIndex && i < currentIndex){
+        moveSlide(currentIndex-1);
+        console.log(i + "," + currentIndex)
+    }
+    
+}
 
 function moveSlide(num){
-    slides.style.left = -num * 350 + "px";
+    let move = 0;
+
+    if(currentIndex < num){
+        move = -num * 1050 + "px";
+    }else{
+        move = num * 1050 + "px";
+    }
+    slides.style.left = move;
     currentIndex = num;
 }
-btn.addEventListener('click', function(){
-    if(currentIndex < slideCount-3){
-        moveSlide(currentIndex+1);
-        console.log(currentIndex);
-    }
-    else{
-        moveSlide(0);
-    }
-});
+
+// function pageScroll(i){
+    
+//     if(i != currentIndex){
+//         if( currentIndex == 0 || currentIndex == 2){
+//             currentIndex = 0;
+//             moveSlide(currentIndex+1);
+//         }else if( currentIndex == 1 || currentIndex == 2){
+//             moveSlide(currentIndex+1);
+//         }else{
+//             moveSlide(currentIndex+1);
+//         }
+//     }
+// }
+
+// function moveSlide(num){
+//     if(num == 0){
+//         slides.style.left = 0+ "px";
+//         currentIndex = num;
+//     }
+//     else if(num == 1){
+//         slides.style.left = -1050+ "px";
+//         currentIndex = num;
+//     }
+//     else if(num == 2){
+//         slides.style.left = -2100 + "px";
+//         currentIndex=num;
+//     }
+//     console.log("num = " + num + " , " + "currentIndex= " + currentIndex  )
+// }
+
+    
